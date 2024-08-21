57d3f70665b1e560d9473d1b5dc27962a59aa09a
Giulio M <1172898+giuliom@users.noreply.github.com>
2024-08-21 11:08:25 +0100

    Folder analysis implemented

--- src/rsync/analyze.rs
+++ src/rsync/analyze.rs
@@ -1,9 +1,7 @@
-use chrono::{DateTime, Utc};
-use std::fs::File;
-use std::io::{Error, ErrorKind};
+use chrono;
+use std::io::ErrorKind;
 use std::path::Path;
-use super::filesystem::{ContentTree, FolderData, FileData};
-use super::crypto;
+use super::filesystem::{ContentTree, FileData, FolderData};
 
 pub fn read_folder_content(path: &String) -> Result<ContentTree, std::io::Error>{
     let dir = Path::new(path);
@@ -14,31 +12,40 @@ pub fn read_folder_content(path: &String) -> Result<ContentTree, std::io::Error>
             ErrorKind::InvalidInput,
             format!("The path '{}' is not a directory", path),
         ));
-    }   
-
-    let metadata = std::fs::metadata(dir)?;
-    let folder_name = dir.file_name().expect("Invalid path").to_str().expect("Invalid folder name").to_string();
-    let last_modified: DateTime<Utc> = metadata.modified()?.into();
-    
-    let mut root = FolderData::new(folder_name, path.clone(), last_modified);
+    }
+  
+    let root = FolderData::from_path(dir)?;
     let mut content = ContentTree::new(root, chrono::Utc::now());
 
-    // TODO reorganise and properly implement algorithm
-    for entry in std::fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() {
-           //TODO
-        } else if path.is_file() {
-            let file = File::open( entry.path().display().to_string())?;
-            let hash = crypto::generate_hash(&file)?;
-            println!("Hash: {}", hash);
-            content.root.file_hashes.push(hash.clone());
-            let f = FileData::new(entry.file_name().to_str().unwrap().to_string(), entry.path().display().to_string(), "todo".to_string(), hash.clone(), Utc::now());
-            content.files.insert(hash, f);
+    let mut folders: Vec<&mut FolderData> = Vec::new();
+    folders.push(&mut content.root);
+
+    while folders.len() > 0 {
+        let folder = folders.pop().unwrap();
+        let mut subfolders: Vec<FolderData> = Vec::new();
+
+        // TODO reorganise and properly implement algorithm
+        for entry in std::fs::read_dir(&folder.path)? {
+            let entry = entry?;
+            let path = entry.path();
+            
+            if path.is_dir() {
+                let sub_folder = FolderData::from_dir(&entry)?;
+                subfolders.push(sub_folder);
+            } else if path.is_file() {
+                let file_data = FileData::from_dir(&entry)?;
+                folder.file_hashes.push(file_data.hash.clone());
+                content.files.insert(file_data.hash.clone(), file_data);
+            }
+        }
+
+        folder.subfolders = subfolders;
+        
+        for fd in &mut folder.subfolders {
+            folders.push(fd);
         }
     }
-    
+
     Ok(content)
 }
 
--- src/rsync/filesystem.rs
+++ src/rsync/filesystem.rs
@@ -1,7 +1,8 @@
 use chrono::{DateTime, Utc};
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::{DirEntry, File}, path::Path, time::SystemTime};
+use super::crypto;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct FolderData {
     pub name: String,
     pub path: String,
@@ -14,9 +15,28 @@ impl FolderData {
     pub fn new(name: String, path: String, last_modified: DateTime<Utc>) -> FolderData {
         FolderData{name, path, last_modified, file_hashes: Vec::new(), subfolders: Vec::new()}
     }
+
+    pub fn from_dir(dir: &DirEntry) -> std::io::Result<FolderData> {
+        assert!(dir.path().is_dir());
+
+        let fd = FolderData::from_path(dir.path().as_path())?;
+        Ok(fd)
+    }
+
+    pub fn from_path(path: &Path) -> std::io::Result<FolderData> {
+        assert!(path.is_dir());
+
+        let metadata = std::fs::metadata(path)?;
+        let name = path.get_name();
+        let path_string = path.to_str().unwrap().to_string();
+        let last_modified: DateTime<Utc> = metadata.modified().unwrap_or(SystemTime::now()).into();
+
+        let fd = FolderData::new(name, path_string, last_modified);
+        Ok(fd)
+    }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct FileData {
     pub name: String,
     pub path: String,
@@ -28,6 +48,21 @@ pub struct FileData {
 impl FileData {
     pub fn new(name: String, path: String, extension: String, hash: String, last_modified: DateTime<Utc>) -> FileData {
         FileData{name, path, extension, hash, last_modified}
+    }
+
+    pub fn from_dir(dir: &DirEntry) -> std::io::Result<FileData> {
+        assert!(dir.path().is_file());
+        let file = File::open(dir.path().display().to_string())?;
+        let metadata = file.metadata()?;
+
+        let name = dir.get_name();
+        let path = dir.path().to_str().unwrap().to_string();
+        let extension = dir.path().extension().unwrap().to_str().unwrap().to_string();
+        let hash = crypto::generate_hash(&file)?;
+        let last_modified =  metadata.modified().unwrap_or(SystemTime::now()).into();
+
+        let fd = FileData::new(name, path, extension, hash, last_modified);
+        Ok(fd)
     }
 }
 
@@ -41,5 +76,21 @@ pub struct ContentTree {
 impl ContentTree {
     pub fn new(root: FolderData, last_analysis: DateTime<Utc>) -> ContentTree {
         ContentTree{root, files: HashMap::new(), last_analysis}
+    }
+}
+
+pub trait GetName {
+    fn get_name(&self) -> String;
+}
+
+impl GetName for Path {
+    fn get_name(&self) -> String {
+        self.file_name().unwrap().to_str().unwrap().to_string()
+    }
+}
+
+impl GetName for DirEntry {
+    fn get_name(&self) -> String {
+        self.file_name().to_str().unwrap().to_string()
     }
 }
